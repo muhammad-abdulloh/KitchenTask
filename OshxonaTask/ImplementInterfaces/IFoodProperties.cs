bd9c6664b01d7b6ade072eb6e5282c8dc46b92d9
Clever Coder <clevercoderr@gmail.com>
2021-12-24 19:05:35 +0500

    Add project files.

---
+++ OshxonaTask/ImplementInterfaces/IFoodProperties.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OshxonaTask.ImplementInterfaces
+{
+    internal interface IFoodProperties
+    {
+        public string foodName { get; set; }
+        public string foodType { get; set; }
+        public string foodPrice { get; set; }
+
+
+    }
+}
